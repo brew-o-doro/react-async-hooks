3238706a25ff2cdc5e3561435c1125ac24c15239
Teodoro Garcia <tedtke514@gmail.com>
2021-04-27 11:14:14 -0500

    client: added controlled form to async hooks

--- src/components/Async.js
+++ src/components/Async.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+
 
 export default function Async() {
+    const [search, setSearch] = useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(search);
+    }
+
     return (
         <>
         <h1>Async React Hooks</h1>
 
-        <form>
-            {/* <input value={search} onChange={} placeholder="Search for Gifs!">
-
-            </input> */}
+        <form onSubmit={onSubmit}>
+            <input value={search} onChange={e => setSearch(e.target.value)} placeholder="Search for Gifs!"/>
+            <button type="submit">Search</button>
 
         </form>
             
