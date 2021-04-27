5a6c6c91f6d150bb7720cebbc04228135198a73e
Teodoro Garcia <tedtke514@gmail.com>
2021-04-27 11:07:43 -0500

    client: set up initial files

---
+++ src/components/Async.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function Async() {
+    return (
+        <>
+        <h1>Async React Hooks</h1>
+
+        <form>
+            {/* <input value={search} onChange={} placeholder="Search for Gifs!">
+
+            </input> */}
+
+        </form>
+            
+        </>
+    )
+}
